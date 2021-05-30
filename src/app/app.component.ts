b6d8cc5ebb47bf056cb284605694ea95663a39ee
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2021-05-30 14:26:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterapp';
+
+  //count number
+  countNumber = 0;
+
+  //resetBtn
+  resetBtn() {
+    this.countNumber = 0;
+  }
+
+  //increaseBtn
+  increaseBtn() {
+    this.countNumber = this.countNumber + 1;
+  }
+
+  //decreaseBtn
+  decreaseBtn() {
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber<0){
+      this.countNumber = 0;
+    }
+  }
+
 }
